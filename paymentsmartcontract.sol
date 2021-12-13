4bb4ff4e77872741c586b0ff308f86e0ab45d22a
giupt <86053202+giupt@users.noreply.github.com>
2021-12-13 13:13:36 +0000

    Update paymentsmartcontract.sol

--- paymentsmartcontract.sol
+++ paymentsmartcontract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.8.2;
 
 import "https://github.com/giupt/BIMvalidation/blob/main/OracleInterface.sol";
 import "https://github.com/giupt/BIMvalidation/blob/main/Ownable.sol";
