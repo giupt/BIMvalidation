bc98b7429abf23efddd4202a2f29155fc84af9e6
giupt <86053202+giupt@users.noreply.github.com>
2022-01-31 14:15:53 +0000

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.17;
 
-// @title Ownable
-// @dev The Ownable contract has an owner address, and provides basic authorization control
-// functions, this simplifies the implementation of "user permissions"
+//@title Ownable
+//@dev The Ownable contract has an owner address, and provides basic authorization control
+//functions, this simplifies the implementation of "user permissions"
 
 contract Ownable {
     address public owner;
